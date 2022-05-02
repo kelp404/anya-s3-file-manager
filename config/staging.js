9e2bd5c9ed98253f8009c09e4ed1cb8d12877c66
Kelp <kelp@phate.org>
2022-05-02 12:31:21 +0800

    chore: add S3 secret config

--- config/staging.js
+++ config/staging.js
@@ -1,4 +1,14 @@
 module.exports = {
+	// Direct set S3 secret at here to replace typing S3 secret on the browser.
+	// Please DON'T public anya s3 file manager.
+	// Please DON'T commit S3 secret to the public repository.
+	S3: {
+		KEY: null,
+		SECRET: null,
+		BUCKET: null,
+		REGION: null,
+	},
+
 	MODE: 'staging',
 	IS_USE_DEBUG_ASSETS: false,
 	ASSETS_PATH: '/assets',
